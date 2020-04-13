67a4ff3e7fbafaf415556b28c41a1f7aab1484b2
Venancio Mitidieri <venanciomitidieri@gmail.com>
2020-04-13 14:37:31 -0300

    Resolução dos Exercícios

--- google-python-exercises/basic/string1.py
+++ google-python-exercises/basic/string1.py
@@ -24,7 +24,11 @@
 # So donuts(5) returns 'Number of donuts: 5'
 # and donuts(23) returns 'Number of donuts: many'
 def donuts(count):
-    return 'Number of donuts: ' + (str(count) if count < 10 else 'many')
+    # return 'Number of donuts: ' + (str(count) if count < 10 else 'many')
+    if count < 10:
+        return f'Number of donuts: {count}'
+    else:
+        return 'Number of donuts: many'
 
 # B. both_ends
 # Given a string s, return a string made of the first 2
@@ -32,8 +36,11 @@ def donuts(count):
 # so 'spring' yields 'spng'. However, if the string length
 # is less than 2, return instead the empty string.
 def both_ends(s):
-    # +++your code here+++
-    return
+    # return s[:2] + s[-2:] if len(s) >= 2 else ''
+     if len(s) < 2:
+        return ''
+     if len(s) >= 2:
+        return s[:2] + s[-2:]
 
 
 # C. fix_start
@@ -46,8 +53,7 @@ def both_ends(s):
 # Hint: s.replace(stra, strb) returns a version of string s
 # where all instances of stra have been replaced by strb.
 def fix_start(s):
-    # +++your code here+++
-    return
+    return s[0] + s[1:].replace(s[0], '*')
 
 
 # D. MixUp
@@ -58,8 +64,7 @@ def fix_start(s):
 #   'dog', 'dinner' -> 'dig donner'
 # Assume a and b are length 2 or more.
 def mix_up(a, b):
-    # +++your code here+++
-    return
+    return b[:2] + a[2:] + ' ' + a[:2] + b[2:]
 
 
 # Provided simple test() function used in main() to print

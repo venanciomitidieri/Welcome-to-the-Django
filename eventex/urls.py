690146b140a73ab8c35b7ffff1e59d6e6f794119
Venancio Mitidieri <venanciomitidieri@gmail.com>
2021-02-16 21:36:39 -0300

    Tela de inscrição

--- eventex/urls.py
+++ eventex/urls.py
@@ -17,8 +17,11 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from eventex.core.views import home
 
+from eventex.subscriptions.views import subscribe
+
 
 urlpatterns = [
     url(r'^$', home),
+    url('inscricao/', subscribe),
     url(r'^admin/', admin.site.urls),
 ]
